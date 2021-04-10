31d4ed4841cc8d5d5c1e6be8ffc35f81bff7e143
Tim Coalson <tsc5yc@mst.edu>
2021-04-09 20:35:00 -0500

    reimplement writing direct to file, improve error messages

--- @xmltree/save.m
+++ @xmltree/save.m
@@ -11,7 +11,8 @@ function varargout = save(tree,filename,varargin)
 %             (if requested or if filename is not provided or empty)
 %
 % Convert an XML tree into a well-formed XML string and write it into
-% a file or return it as a string if no filename is provided.
+% a file or return it as a string if no filename is provided, or
+% filename is empty.
 %
 %  See also XMLTREE
 
@@ -20,49 +21,76 @@ prolog = '<?xml version="1.0" ?>\n';
 
 order  = 0;
 if mod(numel(varargin),2)
-    error('[XMLTree] Invalid syntax.');
+    error('[XMLTree] Invalid number of arguments.');
 end
 for i=1:2:numel(varargin)
-    switch lower(varargin{i})
-        case 'prettyprint'
-            if ~varargin{i+1}
-                order = -1;
-            end
-        otherwise
-            error('[XMLTree] Invalid syntax.');
+    if ischar(varargin{i})
+        switch lower(varargin{i})
+            case 'prettyprint'
+                if ~varargin{i+1}
+                    indorder = -1;
+                end
+            otherwise
+                error(['[XMLTree] Unrecognized option "' varargin{i} '".']);
+        end
+    else
+        error(['[XMLTree] Option names must be strings.']); %"char vectors" may be too technical
     end
 end
-
-xmlstr = print_subtree(tree,root(tree),order);
-
-%- Return the XML tree as a string
-if nargin == 1 || isempty(filename)
-    varargout{1} = [sprintf(prolog) xmlstr];
-%- Output specified
-else
+%octave can't make handles to nested functions, while matlab can't call nested functions from non-nested
+%we don't want the implementation to be nested, because accidental variable name reuse can cause disaster
+%so use globals rather than detecting octave and using a shim
+global dofile outbuf fid;
+dofile = false;
+outbuf = {};
+if nargin > 1 && ~isempty(filename)
+    dofile = true;
     %- Filename provided
     if ischar(filename)
         [fid, msg] = fopen(filename,'w');
         if fid==-1, error(msg); end
+        cleanupObj = onCleanup(@()fclose(fid)); %now we don't have to remember to fclose later
         if isempty(tree.filename), tree.filename = filename; end
     %- File identifier provided
     elseif isnumeric(filename) && numel(filename) == 1
         fid = filename;
         prolog = ''; %- With this option, do not write any prolog
     else
-        error('[XMLTree] Invalid argument.');
-    end
-    fprintf(fid,'%s',[sprintf(prolog) xmlstr]);
-    if ischar(filename), fclose(fid); end
-    
-    if nargout == 1
-        varargout{1} = [sprintf(prolog) xmlstr];
+        error('[XMLTree] Invalid argument for filename.');
     end
 end
 
-%==========================================================================
-function xmlstr = print_subtree(tree,uid,order)
+addstring(sprintf(prolog));
+write_subtree(tree, root(tree), order);
 
+if dofile
+    %keep previous behavior: if nargout is 1, *also* generate string version even if generating file version
+    if nargout == 1
+        dofile = false;
+        %addstring(prolog); %2.1 never put prolog in for this case, and note that using fid argument will have erased the prolog string
+        write_subtree(tree, root(tree), order);
+        varargout{1} = [outbuf{:}];
+    end
+else
+    varargout{1} = [outbuf{:}];
+end
+
+clear global dofile outbuf fid;
+%end of save()
+
+
+%==========================================================================
+function addstring(instring)
+global dofile outbuf fid;
+if dofile
+    fprintf(fid, '%s', instring);
+else
+    outbuf{end + 1} = instring;
+end
+
+
+%==========================================================================
+function write_subtree(tree, uid, order)
 if ~strcmp(tree.tree{uid}.type, 'element')
     error('[XMLTree] Input has to be an element.');
 end
@@ -71,49 +99,48 @@ if nargin < 3, order = 0; end
 indentstr      = '';
 closeindentstr = '';
 if order < 0
-    neworder   = order;
+    neworder   = order; %the "order = -1" trick for no formatting
 else
     neworder   = order + 1;
     indentstr  = [sprintf('\n') blanks(3 * neworder)];
     closeindentstr = [sprintf('\n') blanks(3 * order)];
 end
-% Make contents of tag first, then decide what formatting to do after we
+% We can write the contents of tag first, then decide what formatting to do after we
 % know whether there are any tag-like children
-contents = '';
-allchildrentext = true;
+addstring(['<' tree.tree{uid}.name]);
+for i = 1:numel(tree.tree{uid}.attributes)
+    addstring([' ' tree.tree{uid}.attributes{i}.key '="' tree.tree{uid}.attributes{i}.val '"']);
+end
+if isempty(tree.tree{uid}.contents)
+    addstring(' />');
+    return; %clearer than having the rest in an else?
+end
+addstring('>');
+allchildrentext = true; %need to track this for whether to indent the closing tag
 for child_uid = tree.tree{uid}.contents
     switch tree.tree{child_uid}.type
         case 'element'
             allchildrentext = false;
-            contents = [contents indentstr print_subtree(tree, child_uid, neworder)];
+            addstring(indentstr);
+            write_subtree(tree, child_uid, neworder);
         case 'chardata'
-            contents = [contents entity(tree.tree{child_uid}.value)];
+            addstring([entity(tree.tree{child_uid}.value)]);
         case 'cdata'
-            contents = [contents '<![CDATA[' cdata(tree.tree{child_uid}.value) ']]>'];
+            addstring(['<![CDATA[' cdata(tree.tree{child_uid}.value) ']]>']);
         case 'pi'
             allchildrentext = false;
-            contents = [contents indentstr '<?' tree.tree{child_uid}.target ' ' tree.tree{child_uid}.value '?>'];
+            addstring([indentstr '<?' tree.tree{child_uid}.target ' ' tree.tree{child_uid}.value '?>']);
         case 'comment'
             allchildrentext = false;
-            contents = [contents indentstr '<!-- ' tree.tree{child_uid}.value ' -->'];
+            addstring([indentstr '<!-- ' tree.tree{child_uid}.value ' -->']);
         otherwise
             warning('Type %s unknown: not saved', tree.tree{child_uid}.type);
     end
 end
-tagstr = ['<' tree.tree{uid}.name];
-for i = 1:numel(tree.tree{uid}.attributes)
-    tagstr = [tagstr ' ' tree.tree{uid}.attributes{i}.key '="' tree.tree{uid}.attributes{i}.val '"'];
+if ~allchildrentext
+    addstring(closeindentstr);
 end
-%tagstr isn't quite finished, but build xmlstr directly with it to save a little time
-if isempty(tree.tree{uid}.contents)
-    xmlstr = [tagstr ' />'];
-else
-    if allchildrentext
-        xmlstr = [tagstr '>' contents '</' tree.tree{uid}.name '>'];
-    else
-        xmlstr = [tagstr '>' contents closeindentstr '</' tree.tree{uid}.name '>'];
-    end
-end
+addstring(['</' tree.tree{uid}.name '>']);
 
 
 %==========================================================================
